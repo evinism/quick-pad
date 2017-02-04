b643210909259e03cc1659cd91cd1f726f9cb145
Evin Sellin <evinism@gmail.com>
2017-02-04 13:26:00 -0800

    move to inline svg for icon

--- src/client/templates/main.js
+++ src/client/templates/main.js
@@ -27,7 +27,12 @@ module.exports = ({
   </head>
   <body>
     <textarea ${autofocus ? 'autofocus' : ''} id="notepad" name="notepad">${content}</textarea>
-    <a class="new-note" href='/new/' target="_blank">+</a>
+    <a class="new-note" href='/new/' target="_blank">
+      <svg>
+        <line x1="0" y1="8" x2="16" y2="8" stroke="#000" stroke-width="3"/>
+        <line x1="8" y1="0" x2="8" y2="16" stroke="#000" stroke-width="3"/>
+      </svg>
+    </a>
     <script>
       const Environment = ${JSON.stringify({readOnly, noteId, autofocus})};
       ${jsString}
