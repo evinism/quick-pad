1e39b458220c9ce7d568977e4439dbe32e0fe045
Evin Sellin <evinism@gmail.com>
2017-02-10 22:58:03 -0800

    fix dumb width/height issue with plus icon

--- src/client/templates/main.js
+++ src/client/templates/main.js
@@ -28,7 +28,7 @@ module.exports = ({
   <body>
     <textarea ${autofocus ? 'autofocus' : ''} id="notepad" name="notepad">${content}</textarea>
     <a class="new-note" href='/new/' target="_blank">
-      <svg>
+      <svg width="16" height="16">
         <line x1="0" y1="8" x2="16" y2="8" stroke="#000" stroke-width="3"/>
         <line x1="8" y1="0" x2="8" y2="16" stroke="#000" stroke-width="3"/>
       </svg>
