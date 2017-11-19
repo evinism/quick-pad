a8476ee43f321cc0d8ad568375e31068b86755f8
Evin Sellin <evinism@gmail.com>
2017-11-19 11:30:19 -0800

    force ssl

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+var enforce = require('express-sslify');
 const configureRoutes = require('./routes.js');
 const initSockets = require('./socket.js');
 const {initDb} = require('./store.js');
@@ -13,6 +14,11 @@ function run() {
   // Main app!
   var app = express();
   app.set('port', (process.env.PORT || 8080));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  }
+
   app.use(bodyParser.json());
   app.use(express.static('public'));
   configureRoutes(app);
